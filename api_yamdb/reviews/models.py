e6837ae32b4b82db1d0c39fe9110850621ff6d18
Александр Хоменко <alkh0304@yandex.ru>
2022-04-01 13:37:05 +0300

    Добавил модель Review и Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -17,10 +18,51 @@ class Title(models.Model):
     genre = models.ForeignKey(
         Genre,
         on_delete=models.SET_NULL,
-        related_name='titles'
+        related_name='titles',
+        null=True
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
-        related_name='titles'
+        related_name='titles',
+        null=True
+    )
+
+
+class Review(models.Model):
+    reviewed_item = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews')
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    item_rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации отзыва',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=[
+                'reviewed_item',
+                'item_rating',
+                # 'author'
+            ], name='unique_rating')
+        ]
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField()
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(
+        'Дата публикации комментария',
+        auto_now_add=True,
     )
